55bc17afc973487d78a1fce1fee338e5b79f9abc
erickfrodrigues <erickfrodrigues@hotmail.com>
2024-11-02 19:05:22 -0300

    perfilviewModel parte 2

--- AppEmagrecaApareca/Mvvm/ViewModels/PerfilUsuViewModel.cs
+++ AppEmagrecaApareca/Mvvm/ViewModels/PerfilUsuViewModel.cs
@@ -1,4 +1,7 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using AppEmagrecaApareca.Mvvm.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +12,7 @@ namespace AppEmagrecaApareca.Mvvm.ViewModels
 {
     public partial class PerfilUsuViewModel:ObservableObject
     {
+        #region Propriedades do MVVm
 
         [ObservableProperty]
         private int _id;
@@ -19,6 +23,7 @@ namespace AppEmagrecaApareca.Mvvm.ViewModels
         [ObservableProperty]
         private string _sexo;
         [ObservableProperty]
+
         private int _idSexo;
         [ObservableProperty]
         private decimal _altura;
@@ -29,6 +34,88 @@ namespace AppEmagrecaApareca.Mvvm.ViewModels
         [ObservableProperty]
         private decimal _recomendacaoPeso;
 
+        #endregion
+        private int _IdPesquisar;
+        public List<SexoModel> ListarSexo { get; set; }
+        public PerfilUsuario CadUsuario { get; set; }   
 
+        public PerfilUsuViewModel()
+        {
+            ListarSexo = new SexoModel().lSexo() ;
+            ListarUsuario();
+        }
+
+        public void ListarUsuario()
+        {
+            var Contexto = App._Contexto;
+            try
+            {
+                _IdPesquisar = Contexto.PerfilUsuarios.Where(s => s.Id > 0).FirstOrDefault().Id;
+            }
+            catch (Exception)
+            {
+
+                //throw;
+            }
+
+            if (_IdPesquisar > 0)
+            {
+                CadUsuario = Contexto.PerfilUsuarios.Where(s => s.Id == _IdPesquisar).FirstOrDefault();
+                Altura = CadUsuario.Altura;
+                Idade = CadUsuario.Idade;
+                Peso = CadUsuario.Peso;
+                RecomendacaoCalorica = CadUsuario.RecomendacaoCalorica;
+                RecomendacaoPeso = CadUsuario.RecomendacaoPeso;
+                Sexo = CadUsuario.Sexo;
+                IdSexo = ListarSexo.Where(s=>s.DsSexo == Sexo).FirstOrDefault().Id;        
+                Nome = CadUsuario.Nome;
+                Idade = CadUsuario.Idade;
+
+            }
+
+        }
+        [RelayCommand]
+        public void SalvarAlterar(string _Sexo)
+        {
+           
+            var Contexto = App._Contexto;
+
+
+            if (_IdPesquisar != 0)
+            {
+                var obj = Contexto.PerfilUsuarios.Where(s => s.Id == _IdPesquisar).FirstOrDefault();
+                obj.Id = _IdPesquisar;
+                obj.Altura = Altura;
+                obj.Idade = Idade;
+                obj.Nome = Nome;
+                obj.Sexo = _Sexo;
+                obj.IdSexo = ListarSexo.Where(s => s.DsSexo == _Sexo).FirstOrDefault().Id;
+                obj.RecomendacaoCalorica = RecomendacaoCalorica;
+                obj.RecomendacaoPeso = RecomendacaoPeso;
+
+                Contexto.Entry(obj).State = EntityState.Modified;
+
+            }
+            else
+            {
+                PerfilUsuario perfil = new PerfilUsuario()
+                {
+                    Altura = Altura,
+                    Idade = Idade,
+                    Nome = Nome,
+                    Peso = Peso,
+                    RecomendacaoCalorica = RecomendacaoCalorica,
+                    RecomendacaoPeso = RecomendacaoPeso,                   
+                    Sexo = _Sexo,
+                    IdSexo = ListarSexo.Where(s => s.DsSexo == _Sexo).FirstOrDefault().Id
+
+                };
+                Contexto.PerfilUsuarios.Add(perfil);
+            }
+
+            Contexto.SaveChanges();
+
+            App.Current.MainPage.DisplayAlert("Registro salvo", "Perfil salvo com sucesso!", "ok");
+        }
     }
 }
--- AppEmagrecaApareca/Mvvm/Views/PerfilUsuView.xaml.cs
+++ AppEmagrecaApareca/Mvvm/Views/PerfilUsuView.xaml.cs
@@ -15,40 +15,9 @@ public partial class PerfilUsuView : ContentPage
 	{
 		InitializeComponent();
 		BindingContext = new PerfilUsuViewModel();
-		CarregarTipos();
-		var Contexto = App._Contexto;
-        try
-		{
-            _id = Contexto.PerfilUsuarios.Where(s => s.Id > 0).FirstOrDefault().Id;
-        }
-		catch (Exception)
-		{
-
-			//throw;
-		}
-
-        if (_id > 0)
-        {
-            var obj = Contexto.PerfilUsuarios.Where(s => s.Id == _id).FirstOrDefault();
-            Altura.Text= obj.Altura.ToString();
-            Idade.Text=obj.Idade.ToString();
-            Peso.Text = obj.Peso.ToString();
-            RecomendacaoCalorica.Text = obj.RecomendacaoCalorica.ToString();
-            RecomendacaoPeso.Text = obj.RecomendacaoPeso.ToString();
-			pickerSexo.SelectedIndex = obj.IdSexo;
-            Nome.Text = obj.Nome.ToString();
-            Idade.Text = obj.Idade.ToString();
-
-        }
+		
 
     }
-
-	public void CarregarTipos()
-	{
-		pickerSexo.Title = "Selecione sexo";		
-		pickerSexo.ItemsSource = new SexoModel().lSexo();
-		pickerSexo.ItemDisplayBinding=new Binding("DsSexo");
-	}
 
 	void SelSexo(object sender, EventArgs e)
 	{
@@ -59,63 +28,15 @@ public partial class PerfilUsuView : ContentPage
 		{
 			SexoModel sexo=(SexoModel)pickerSexo.ItemsSource[selectIndex];
 			_Idsexo = sexo.Id;
-			STipoSexo = sexo.DsSexo;
-		
-		}
-
-    }
-
-	private async void OnAlterar(object sender,EventArgs e)
-	{
-		int _id = 0;
-		var Contexto=App._Contexto;
-
-		try
-		{
-            _id = Contexto.PerfilUsuarios.Where(s => s.Id > 0).FirstOrDefault().Id;
-        }
-		catch (Exception)
-		{
-
 			
-		}
+			eds_sexo.Text = sexo.DsSexo;
 
-		if (_id!=0)
-		{
-			var obj=Contexto.PerfilUsuarios.Where(s=>s.Id==_id).FirstOrDefault();	
-			obj.Altura=Convert.ToDecimal(Altura.Text);
-            obj.Idade = Convert.ToInt32(Idade.Text);
-			obj.Nome= Nome.Text;
-			obj.Sexo = STipoSexo;
-			obj.IdSexo = _Idsexo;
-            obj.RecomendacaoCalorica = Convert.ToDecimal(RecomendacaoCalorica.Text);
-            obj.RecomendacaoPeso = Convert.ToDecimal(RecomendacaoPeso.Text);
-
-			Contexto.Entry(obj).State=EntityState.Modified;
 
         }
-		else
-		{
-			PerfilUsuario perfil = new PerfilUsuario()
-			{
-				Altura= Convert.ToDecimal(Altura.Text),
-                Idade = Convert.ToInt32(Idade.Text),
-                Nome = Nome.Text,
-                Peso = Convert.ToDecimal(Peso.Text),
-                RecomendacaoCalorica = Convert.ToDecimal(RecomendacaoCalorica.Text),
-                RecomendacaoPeso = Convert.ToDecimal(RecomendacaoPeso.Text),
-				IdSexo=_Idsexo,
-				Sexo=STipoSexo
 
-            };
-			Contexto.PerfilUsuarios.Add(perfil);
-		}
-
-		Contexto.SaveChanges();
-
-        DisplayAlert("Registro salvo", "Perfil salvo com sucesso!", "ok");
     }
 
+	
 	  
 	
 		
