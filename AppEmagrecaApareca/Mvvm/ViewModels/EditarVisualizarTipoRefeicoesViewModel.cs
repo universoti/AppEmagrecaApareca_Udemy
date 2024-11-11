b1274a2cea4d009d13e22e0e614c32b305ec6590
erickfrodrigues <erickfrodrigues@hotmail.com>
2024-11-11 20:07:27 -0300

    Maui3_aula26_p2

--- AppEmagrecaApareca/Mvvm/ViewModels/EditarVisualizarTipoRefeicoesViewModel.cs
+++ AppEmagrecaApareca/Mvvm/ViewModels/EditarVisualizarTipoRefeicoesViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using AppEmagrecaApareca.Mvvm.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,6 +31,40 @@ namespace AppEmagrecaApareca.Mvvm.ViewModels
 
         [ObservableProperty]
         private string _msgRetorno;
+
+        [RelayCommand]
+        private void CadastrarAtualizar()
+        {
+            var Contexto = App._Contexto;
+
+            if (Id == 0)
+            {
+                TipoRefeicao tipoRefeicoes = new TipoRefeicao();
+                tipoRefeicoes.DsTipoRefeicao = DsTipoRefeicao;
+                tipoRefeicoes.Id = Id;
+
+                Contexto.Add(tipoRefeicoes);
+
+            }
+            else
+            {
+                var obj = Contexto.TipoRefeicoes.Where(s => s.Id == Id).FirstOrDefault();
+                obj.DsTipoRefeicao = DsTipoRefeicao;
+                Contexto.Entry(obj).State = EntityState.Modified;
+
+
+            }
+            Contexto.SaveChanges();
+            MsgRetorno = "Lançamento salvo com sucesso!";
+            Shell.Current.DisplayAlert("Registro salvo", "Lançamento salvo com sucesso!", "OK");
+
+            var stack = Shell.Current.Navigation.NavigationStack.ToArray();
+            for (int i = stack.Length - 1; i > 0;i--)
+            {
+                Shell.Current.Navigation.RemovePage(stack[i]);
+            }
+            Shell.Current.GoToAsync("//TelaInicio");
+        }
 
     }
 }
